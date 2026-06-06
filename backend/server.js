96006011e109ea71ce4fce6bc9fc5699692863b9
Abdul Ayub Ali <abdulayubali004@gmail.com>
2026-06-06 09:25:53 +0530

    User Schema Setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 import "dotenv/config";
 import app from "./src/app.js";
 import connectDb from "./src/config/database.js";
+
 const port = process.env.PORT || 3000;
+
+// Connect to Database
 connectDb();
+
+// Start Server
+app.js
 app.listen(port, () => {
-  console.log(`Server is running at port:${port}`);
+  console.log(`Server is running at port: ${port}`);
 });
---
+++ backend/src/models/User.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs'
+
+//User Schema for authentication
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true }); // for getting 'createdAt' aur 'updatedAt' automatically
+
+// for encrypt passod before saving in databse
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+// For matching password
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
